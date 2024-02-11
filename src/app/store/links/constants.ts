bcf448c36eea6aca82ff415e7265ceb752d8951e
vipin_760 <vipinm500@gmail.com>
2024-02-11 16:41:47 +0530

    base url setup

--- src/app/store/links/constants.ts
+++ src/app/store/links/constants.ts
@@ -1,2 +1,2 @@
-// export const BASE_URI = "https://url-shortener-2-backend.vercel.app/api/url/"
- export const BASE_URI ="http://localhost:3000/api/url"
+ export const BASE_URI = "https://url-shortener-2-backend.vercel.app/api/url"
+// export const BASE_URI ="http://localhost:3000/api/url"
